9020ee53457a636766fee2fc44967866d4edfa1d
BartlomiejSzady <122043042+BartlomiejSzady@users.noreply.github.com>
2023-01-06 15:59:58 +0100

    Update cenyafter.php

--- cenyafter.php
+++ cenyafter.php
@@ -27,6 +27,24 @@
     $kosztżabka = $kosztżabka + 3.6;
     $kosztbiedronka = $kosztbiedronka + 4.3;
   }
+
+  if (isset($_POST['chlebt'])) {
+    $kosztlidl = $kosztlidl + 5.1;
+    $kosztżabka = $kosztżabka + 3.6;
+    $kosztbiedronka = $kosztbiedronka + 4.3;
+  }
+
+  if (isset($_POST['pomidor'])) {
+    $kosztlidl = $kosztlidl + 5.1;
+    $kosztżabka = $kosztżabka + 3.6;
+    $kosztbiedronka = $kosztbiedronka + 4.3;
+  }
+
+  if (isset($_POST['hummus'])) {
+    $kosztlidl = $kosztlidl + 5.1;
+    $kosztżabka = $kosztżabka + 3.6;
+    $kosztbiedronka = $kosztbiedronka + 4.3;
+  }
 ?> 
   
   <div class="middleafter"><h1>Za zakupy zapłacisz:</h1></div>
@@ -35,7 +53,7 @@
   <div class="żabka">Za zakupy w Żabce zapłacisz <strong><?php echo $kosztżabka?> </strong> pln.</div>
   <div class="lidllogo"><img src="img/lidl.jpg" alt="lidl" width="150" height="150"></div>
   <div class="lidl">Za zakupy w Lidlu zapłacisz <strong><?php echo $kosztlidl?> </strong> pln.</div>
-  <div class="biedronkalogo"><img src="img/biedronka.jpg" alt="biedronka" width="180" height="150"></div>
+  <div class="biedronkalogo"><img src="img/biedronka.jpg" alt="biedronka" width="235" height="150"></div>
   <div class="biedronka">Za zakupy w Biedronce zapłacisz <strong><?php echo $kosztbiedronka?> </strong> pln. </div>
     
 </body>
