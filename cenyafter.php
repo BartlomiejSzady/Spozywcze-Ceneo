475123409cfc4dbcff47e23d9a5278a3c447f2d2
BartlomiejSzady <122043042+BartlomiejSzady@users.noreply.github.com>
2023-01-07 22:35:56 +0100

    Update cenyafter.php

--- cenyafter.php
+++ cenyafter.php
@@ -10,43 +10,51 @@
   $kosztżabka = 0;
   $kosztbiedronka = 0;
 
-  if (isset($_POST['mleko'])) {
-    $kosztlidl = $kosztlidl + 4.3;
-    $kosztżabka = $kosztżabka + 3.8;
-    $kosztbiedronka = $kosztbiedronka + 3.3;
-  }
+  $mlekoszt = ($_POST['mlekoszt']);
+    $kosztlidl = $kosztlidl + $mlekoszt*4.49;
+    $kosztżabka = $kosztżabka + $mlekoszt*4.8;
+    $kosztbiedronka = $kosztbiedronka + $mlekoszt*3.99;
 
-  if (isset($_POST['masło'])) {
-    $kosztlidl = $kosztlidl + 8.3;
-    $kosztżabka = $kosztżabka + 12;
-    $kosztbiedronka = $kosztbiedronka + 9.3;
-  }
+  $masloszt = ($_POST['masłoszt']);
+    $kosztlidl = $kosztlidl + $masloszt*6.99;
+    $kosztżabka = $kosztżabka + $masloszt*8.5;
+    $kosztbiedronka = $kosztbiedronka + $masloszt*6.99;
 
-  if (isset($_POST['szpinak'])) {
-    $kosztlidl = $kosztlidl + 5.1;
-    $kosztżabka = $kosztżabka + 3.6;
-    $kosztbiedronka = $kosztbiedronka + 4.3;
-  }
+    $szpinakszt = ($_POST['szpinakszt']);
+    $kosztlidl = $kosztlidl + $szpinakszt*3.35;
+    $kosztżabka = $kosztżabka + $szpinakszt*3.99;
+    $kosztbiedronka = $kosztbiedronka + $szpinakszt*3.19;
 
-  if (isset($_POST['chlebt'])) {
-    $kosztlidl = $kosztlidl + 5.1;
-    $kosztżabka = $kosztżabka + 3.6;
-    $kosztbiedronka = $kosztbiedronka + 4.3;
-  }
+    $chlebtszt = ($_POST['chlebtszt']);
+    $kosztlidl = $kosztlidl + $chlebtszt*4.68;
+    $kosztżabka = $kosztżabka + $chlebtszt*6.39;
+    $kosztbiedronka = $kosztbiedronka + $chlebtszt*4.66;
 
-  if (isset($_POST['pomidor'])) {
-    $kosztlidl = $kosztlidl + 5.1;
-    $kosztżabka = $kosztżabka + 3.6;
-    $kosztbiedronka = $kosztbiedronka + 4.3;
-  }
+    $czekoladaszt = ($_POST['czekoladaszt']);
+    $kosztlidl = $kosztlidl + $czekoladaszt*4.43;
+    $kosztżabka = $kosztżabka + $czekoladaszt*6.11;
+    $kosztbiedronka = $kosztbiedronka + $czekoladaszt*3.99;
 
-  if (isset($_POST['hummus'])) {
-    $kosztlidl = $kosztlidl + 5.1;
-    $kosztżabka = $kosztżabka + 3.6;
-    $kosztbiedronka = $kosztbiedronka + 4.3;
-  }
+    $sólszt = ($_POST['sólszt']);
+    $kosztlidl = $kosztlidl + $masloszt*1.29;
+    $kosztżabka = $kosztżabka + $masloszt*3.2;
+    $kosztbiedronka = $kosztbiedronka + $masloszt*1.29;
+
+    $olejszt = ($_POST['olejszt']);
+    $kosztlidl = $kosztlidl + $masloszt*12.99;
+    $kosztżabka = $kosztżabka + $masloszt*14.99;
+    $kosztbiedronka = $kosztbiedronka + $masloszt*12.99;
+
+    $jajkaszt = ($_POST['jajkaszt']);
+    $kosztlidl = $kosztlidl + $jajkaszt*7.49;
+    $kosztżabka = $kosztżabka + $jajkaszt*9.99;
+    $kosztbiedronka = $kosztbiedronka + $jajkaszt*7.99;
+
+    $bananyszt = ($_POST['bananyszt']);
+    $kosztlidl = $kosztlidl + $bananyszt*6.99;
+    $kosztżabka = $kosztżabka + $bananyszt*6.99;
+    $kosztbiedronka = $kosztbiedronka + $bananyszt*6.99;
 ?> 
-  
   <div class="middleafter"><h1>Za zakupy zapłacisz:</h1></div>
   <hr> <br>
   <div class="żabkalogo"><img src="img/żabka.jpg" alt="żabka" width="180" height="120"></div>
@@ -54,7 +62,6 @@
   <div class="lidllogo"><img src="img/lidl.jpg" alt="lidl" width="150" height="150"></div>
   <div class="lidl">Za zakupy w Lidlu zapłacisz <strong><?php echo $kosztlidl?> </strong> pln.</div>
   <div class="biedronkalogo"><img src="img/biedronka.jpg" alt="biedronka" width="235" height="150"></div>
-  <div class="biedronka">Za zakupy w Biedronce zapłacisz <strong><?php echo $kosztbiedronka?> </strong> pln. </div>
-    
+  <div class="biedronka">Za zakupy w Biedronce zapłacisz <strong><?php echo $kosztbiedronka?> </strong> pln. </div> 
 </body>
 </html>
