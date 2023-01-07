eaabf5b562e062286b1b0e394f6cb84efbed61c8
BartlomiejSzady <122043042+BartlomiejSzady@users.noreply.github.com>
2023-01-07 22:35:25 +0100

    Update ceny.php

--- ceny.php
+++ ceny.php
@@ -9,17 +9,41 @@
   <div class="middle"><h1>Wybierz produkty których ceny chcesz porównać:</h1></div>
   <form action="cenyafter.php" method="post" class="list">
     <input type="checkbox" id="mleko" name="mleko" value= "mleko">
-    <label for="mleko"> Mleko </label><br>
+    <label for="mleko"> Mleko 3,2% 1l </label>
+    <input type="text" name="mlekoszt" maxlength="2" value="0" id="mlekoszt"> szt.
+    <br>
     <input type="checkbox" id="masło" name="masło" value= "masło">
-    <label for="masło"> Masło </label><br>
+    <label for="masło"> Masło 200g</label>
+    <input type="text" name="masłoszt" maxlength="2" value="0" id="mlekoszt"> szt.
+    <br>
     <input type="checkbox" id="szpinak" name="szpinak" value= "szpinak">
-    <label for="szpinak"> Szpinak </label><br>
-    <input type="checkbox" id="chlebt" name="chlebt" value= "chlebt">
-    <label for="chlebt"> Chleb Tostowy </label><br>
-    <input type="checkbox" id="pomidor" name="pomidor" value= "pomidor">
-    <label for="pomidor"> Pomidor </label><br>
-    <input type="checkbox" id="hummus" name="hummus" value= "hummus">
-    <label for="hummus"> Hummus </label><br><br>
+    <label for="szpinak"> Szpinak 450g</label>
+    <input type="text" name="szpinakszt" maxlength="2" value="0" id="szpinakszt"> szt.
+    <br>
+    <input type="checkbox" id="chlebt" name="chleb" value= "chlebt">
+    <label for="chlebt"> Chleb Tostowy 250g</label>
+    <input type="text" name="chlebtszt" maxlength="2" value="0" id="chlebtszt"> szt.
+    <br>
+    <input type="checkbox" id="czekolada" name="czekolada" value= "czekolada">
+    <label for="czekolada"> Czekolada 100g </label>
+    <input type="text" name="czekoladaszt" maxlength="2" value="0" id="czekoladaszt"> tabliczka.
+    <br>
+    <input type="checkbox" id="sól" name="sól" value= "sól">
+    <label for="sól"> Sól 1kg </label>
+    <input type="text" name="sólszt" maxlength="2" value="0" id="sólszt"> szt.
+    <br>
+    <input type="checkbox" id="olej" name="olej" value= "olej">
+    <label for="olej"> Olej 1l</label>
+    <input type="text" name="olejszt" maxlength="2" value="0" id="olejszt"> szt.
+    <br>
+    <input type="checkbox" id="jajka" name="jajka" value= "jajka">
+    <label for="jajka"> 10 jajek</label>
+    <input type="text" name="jajkaszt" maxlength="2" value="0" id="jajkaszt"> paczka.
+    <br>
+    <input type="checkbox" id="banany" name="banany" value= "banany">
+    <label for="banany"> Banany 1kg </label>
+    <input type="text" name="bananyszt" maxlength="2" value="0" id="bananyszt"> kg.
+    <br><br>
     <input type="submit" value="Porównaj" class="button">
   </form>
 </body>
